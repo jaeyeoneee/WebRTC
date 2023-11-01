ebe6db51a9d7366e6fa3da54e863543d094309b6
jaeyeoneee <ljy8733@dgist.ac.kr>
2023-11-01 18:53:20 +0900

    스프링 부트 초기 설정

---
+++ src/test/java/webrtc/signaling/SignalingApplicationTests.java
@@ -0,0 +1,13 @@
+package webrtc.signaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
