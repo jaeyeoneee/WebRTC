0d67d689ba70afd355a3c3d67c81d203bd3e2560
jaeyeoneee <ljy8733@dgist.ac.kr>
2023-11-05 16:14:07 +0900

    웹캠 컨트롤러

---
+++ src/main/java/webrtc/signaling/controller/WebcamController.java
@@ -0,0 +1,16 @@
+package webrtc.signaling.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class WebcamController {
+
+    @GetMapping("/webcam")
+    public String videoStream() {
+        log.info("/webcam");
+        return "webcam/webcam-streaming";
+    }
+}
