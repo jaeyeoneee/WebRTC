5ebaad83f8a08df02e7baa25f58a51187ff64046
jaeyeoneee <ljy8733@dgist.ac.kr>
2024-01-22 01:31:38 +0900

    feat: subscribe 이벤트리스너 추가

--- src/main/java/webrtc/signaling/event/WebSocketEventListener.java
+++ src/main/java/webrtc/signaling/event/WebSocketEventListener.java
@@ -3,9 +3,11 @@ package webrtc.signaling.event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import webrtc.signaling.repository.WebcamRepository;
 
 @Slf4j
@@ -14,6 +16,14 @@ import webrtc.signaling.repository.WebcamRepository;
 public class WebSocketEventListener {
 
     private final WebcamRepository webcamRepository;
+
+    @EventListener
+    public void handleStompSubscribeListener(SessionSubscribeEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("subscribe={}", event.getMessage().getHeaders());
+    }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
