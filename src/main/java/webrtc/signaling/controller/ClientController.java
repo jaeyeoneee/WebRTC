bad3c25ea87431e7afe5d9097ea8c45af34f2d4f
jaeyeoneee <ljy8733@dgist.ac.kr>
2024-02-06 16:12:19 +0900

    chore: import 삭제

--- src/main/java/webrtc/signaling/controller/ClientController.java
+++ src/main/java/webrtc/signaling/controller/ClientController.java
@@ -2,18 +2,15 @@ package webrtc.signaling.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import webrtc.signaling.domain.Webcam;
-import webrtc.signaling.repository.WebcamKeyRepository;
 import webrtc.signaling.repository.WebcamRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Controller
